8f729f7ecd4fbdec5ffdebe8eab7fc9a6936ccad
Victor Guard <vicstor@bk.ru>
2019-07-31 18:14:48 +0300

    error correction

--- app/controllers/dashboard/courses_controller.rb
+++ app/controllers/dashboard/courses_controller.rb
@@ -7,34 +7,6 @@ class Dashboard::CoursesController < Dashboard::BaseController
     @course_users = course.course_users
   end
 
-  def new
-    @course = type_class.new
-  end
-
-  def create
-    @course = current_user.courses.build(course_params)
-
-    if @course.save
-      flash[:notice] = t(".create")
-      redirect_to dashboard_courses_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-    course.course_users.build unless course.course_users.any?
-   end
-
-  def update
-    if course.update course_params
-      flash[:notice] = t(".update")
-      redirect_to dashboard_courses_path
-    else
-      render :edit
-    end
-  end
-
   def destroy
     course.destroy
 
@@ -48,24 +20,4 @@ class Dashboard::CoursesController < Dashboard::BaseController
     @course ||= current_user.courses.find(params[:id])
   end
   helper_method :course
-
-  def course_params
-    params.require(type.underscore.to_sym).permit(:type, :name, :image,
-     :number_hours_video, :number_hours_practice, :course_start_date,
-      :course_end_date,course_users_attributes: %i[id _destroy user_id],
-       lessons_attributes: %i[id title priority] )
-  end
-
-  def set_type
-    @type = type
-  end
-  helper_method :set_type
-
-  def type
-    Course.types.include?(params[:type]) ? params[:type] : "Course"
-  end
-
-  def type_class
-    type.constantize
-  end
 end
